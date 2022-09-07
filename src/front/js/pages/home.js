c31ab849b178a14b34734106a44b2deccdfa837d
Ksavir <kevinsavir20@gmail.com>
2022-09-07 01:42:40 +0000

    preliminar pagina de perfil, se debe cambiar los componentes Home con el nombre que se usara

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,26 +1,44 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
-import rigoImageUrl from "../../img/rigo-baby.jpg";
+import Stark from "../../img/stark.jpg";
 import "../../styles/home.css";
 
 export const Home = () => {
 	const { store, actions } = useContext(Context);
 
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
-			<div className="alert alert-info">
-				{store.message || "Loading message from the backend (make sure your python backend is running)..."}
+		<div className="d-flex justify-content-around my-4">
+			<div className="d-flex flex-column align-items-center">
+				<img  id="img-perfil" src={Stark} alt="equipo stark"/>
+				<div className="d-flex justify-content-bewteen my-4">
+					<div id="favoritos" className="mx-3"><p>Favoritos</p></div>
+					<div id="interacciones"><p>Ultimas interacciones</p></div>
+				</div>
 			</div>
-			<p>
-				This boilerplate comes with lots of documentation:{" "}
-				<a href="https://github.com/4GeeksAcademy/react-flask-hello/tree/95e0540bd1422249c3004f149825285118594325/docs">
-					Read documentation
-				</a>
-			</p>
+			<form id="formulario" className="">
+			<h3 id="title" className="my-3" >Hola, Usuario</h3>
+			<div className="mb-3">
+				<label id="info-name" for="exampleInputName" className="form-label">Ingresa tu nombre</label>
+				<input type="text" className="form-control" id="name"/>
+				<button type="submit" id="boton">Cambiar</button>
+			</div>
+			<div className="mb-3">
+				<label id="email" for="exampleInputEmail1" className="form-label">Correo Electronico</label>
+				<input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"/>
+				<button type="submit" id="boton">Cambiar</button>
+			</div>
+			<div className="mb-3">
+				<label id="password" for="exampleInputPassword1" className="form-label">Contraseña</label>
+				<input type="password" className="form-control" id="exampleInputPassword1"/>
+				<button type="submit" id="boton">Cambiar</button>
+			</div>
+			<div className="mb-3">
+				<label id="info-number" for="exampleInputNumber" className="form-label">Numero Telefonico</label>
+				<input type="text" className="form-control" id="number" placeholder="+56 9"/>
+				<button type="submit" id="boton">Cambiar</button>
+			</div>
+			<button type="submit" id="boton">Guardar Cambios</button>
+		</form>	
 		</div>
 	);
 };
